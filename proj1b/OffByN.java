5c14843064a39c86e987680f902d4e28f7f938d0
haofumei <haofumei@gmail.com>
2019-05-21 17:46:41 -0700

    2

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+public class OffByN implements CharacterComparator{
 
-public class OffByN {
+    int N;
+    public OffByN(int number) {
+        N = number;
+    }
+
+    public boolean equalChars(char x, char y) {
+        return Math.abs(x - y) == N;
+    }
 }
